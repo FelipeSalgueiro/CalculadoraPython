1747c73d9b4ab67ccda690c2a1c2d1b38d3edcfb
Felipe Maia Salgueiro <felipemaiasalgueiro@gmail.com>
2023-05-01 14:57:10 -0300

    Função Soma

--- Pri_Pro.py
+++ Pri_Pro.py
@@ -1,14 +1,17 @@
+#Varíavel opção usuário
 opc = 1
+
+#Varíaveis a serem somadas/subtraidas/multiplicadas/divididas
 x, y = 0, 0
 
+#Menu
+opc = input("Calculadora: \n1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão\n") #Entrada do usuário
 
-print("Calculadora: \n1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão")
-opc = input()
-
+#Opção Soma
 if opc == "1":
-        x = input("Por favor informe o primeiro número a ser somado: ")
-        y = input("Por favor informe o segundo número a ser somado: ")
-        soma = int(x)+int(y)
-        print("Resultado:", soma)
+        x = input("Por favor informe o primeiro número a ser somado: ") #Coleta do primeiro valor
+        y = input("Por favor informe o segundo número a ser somado: ") #Coleta do segundo valor
+        soma = int(x)+int(y) #Soma dos valores (Int() é necessário pra converter de String para Int)
+        print("Resultado:", soma) #Mostrar resultado final da soma
 else:
-        print("Erro")
+        print("Erro") #Caso pule o if
